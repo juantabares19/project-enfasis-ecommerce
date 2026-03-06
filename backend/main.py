4d4be0500a27da14d9e07d01135c0f7d497b2ad2
Juan Felipe Tabares Serna <juantabares342@gmail.com>
2026-03-06 01:24:16 +0000

    Preparando despliegue para Render

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
-import uvicorn
+from fastapi import FastAPI
 
-if __name__ == '__main__':
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
+# ESTA ES LA LÍNEA QUE TE FALTA O TIENE OTRO NOMBRE
+app = FastAPI() 
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    # Aquí "main:app" busca el archivo "main" y la variable "app"
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
